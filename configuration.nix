928fefbad694ddcff60b90c89d6049b7d9e6ca34
plusgut <github@webentwickler2-0.de>
2022-02-11 20:09:42 +0100

    feat(programms): add general purpose language

--- configuration.nix
+++ configuration.nix
@@ -98,6 +98,8 @@
     brightnessctl
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
+    htop
+    killall
     firefox
     font-awesome
     pamixer
