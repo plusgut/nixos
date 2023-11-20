0ee2fca38a9cb22d2dc378442a2fa72cf7e8f130
plusgut <github@webentwickler2-0.de>
2023-11-20 21:00:46 +0100

    fix(update): add needed whitespace

--- update.sh
+++ update.sh
@@ -1,2 +1,2 @@
-sudo sh -c 'find . -name flake.nix -execdir nix flake --extra-experimental-features nix-command --extra-experimental-features flakes update --commit-lock-file\; ;nixos-rebuild switch --impure --flake ".#"'
+sudo sh -c 'find . -name flake.nix -execdir nix flake --extra-experimental-features nix-command --extra-experimental-features flakes update --commit-lock-file \; ;nixos-rebuild switch --impure --flake ".#"'
 git push
