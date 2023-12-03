392d5de9e309b3cf1be9f1340911ab975ade81cb
plusgut <github@webentwickler2-0.de>
2023-12-03 20:52:56 +0100

    feat(river): change windowmanger to river

--- packages/river/flake.nix
+++ packages/river/flake.nix
@@ -3,8 +3,12 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    ristate = {
+        url = "git+https://gitlab.com/snakedye/ristate.git";
+        flake = false;
+    };
   };
-  outputs = { self, nixpkgs, flake-utils, }:
+  outputs = { self, nixpkgs, flake-utils, ristate }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
@@ -12,6 +16,12 @@
       in {
         packages = {
             default = pkgs.river;
+            ristate = pkgs.rustPlatform.buildRustPackage rec {
+              pname = "ristate";
+              version = "0.1.0";
+              src = ristate;
+              cargoLock.lockFile = ristate + "/Cargo.lock";
+            };
         };
     }
   );
