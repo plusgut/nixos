5d0a51c22bc9ce93d792d5516b751a5fc5c46893
plusgut <github@webentwickler2-0.de>
2023-12-27 08:56:25 +0100

    fix(update): fix update script to handle root better

--- update.sh
+++ update.sh
@@ -1,2 +1,3 @@
-sudo sh -c 'find . -name flake.nix -execdir nix flake --extra-experimental-features nix-command --extra-experimental-features flakes update --commit-lock-file \; ;nixos-rebuild switch --impure --flake ".#"'
+find . -name flake.nix -execdir nix flake --extra-experimental-features nix-command --extra-experimental-features flakes update --commit-lock-file \;
+sudo nixos-rebuild switch --impure --flake ".#"
 git push
