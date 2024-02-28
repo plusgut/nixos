87d201a9c23ee7f68d76e2f1a4734c14f7b14e6e
plusgut <github@webentwickler2-0.de>
2024-02-28 17:24:55 +0100

    feat(qtile): change original repo and add cursor patch

--- packages/qtile/flake.nix
+++ packages/qtile/flake.nix
@@ -1,10 +1,9 @@
-
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     qtile = {
-      url = "github:plusgut/qtile/cursor_size";
+      url = "github:qtile/qtile";
       flake = false;
     };
   };
@@ -13,17 +12,26 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
 
-      in {
+      in
+      {
         packages = {
-            default = pkgs.python3.withPackages (_p: [
-              (pkgs.python3.pkgs.qtile.overrideAttrs { src = qtile; })
-              pkgs.wlroots_0_16
-              pkgs.python3.pkgs.pyxdg
-            ]);
-            swaybg = pkgs.swaybg;
-            swaylock = pkgs.swaylock;
+          default = pkgs.python3.withPackages (_p: [
+            (pkgs.python3.pkgs.qtile.overrideAttrs {
+              src = qtile;
+              patches = [
+                (pkgs.fetchpatch {
+                  url = "https://github.com/plusgut/qtile/commit/4772c2fca9c8570652247ba2c813c414ab5e5f64.patch";
+                  hash = "sha256-vUn1se0coAfkZZ8qwGDLoweLbp2OU+CyUHKsPWzqL0E=";
+                })
+              ];
+            })
+            pkgs.wlroots_0_16
+            pkgs.python3.pkgs.pyxdg
+          ]);
+          swaybg = pkgs.swaybg;
+          swaylock = pkgs.swaylock;
         };
 
-    }
-  );
+      }
+    );
 }
