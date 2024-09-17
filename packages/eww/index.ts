236869965b379f75e0af5b6ed3cea4aa948f4b1c
plusgut <github@webentwickler2-0.de>
2024-09-17 16:31:39 +0200

    feat(eww): niri event handling

--- packages/eww/index.ts
+++ packages/eww/index.ts
@@ -1,9 +1,33 @@
-import { connect } from "node:net";
+import { spawn } from "node:child_process";
 
-const socket = connect(process.env.NIRI_SOCKET as string, () => {
-    console.log("socket!")
-})
+const niri = spawn("niri", ["msg", "--json", "event-stream"], { serialization: "json"})
 
-socket.addListener("data", (msg) => {
-    console.log(msg)
+type NiriEvent =
+    | { WindowFocusChanged: { id: number }}
+    | { WorkspaceActiveWindowChanged: { }}
+
+class UnreachableError extends Error {
+    constructor(opt: never) {
+        super(`Sould not be possible ${opt}`)
+    }
+}
+
+let previousBuffers: Buffer[] = []
+niri.stdout.on("data", (data) => {
+    previousBuffers.push(data)
+    if (data.includes("\n")) {
+        const currentBuffer = Buffer.concat(previousBuffers)
+        previousBuffers = []
+        const msg: NiriEvent = JSON.parse(currentBuffer.toString());
+
+        if ("WindowFocusChanged" in msg) {
+            console.log("focus")
+        } else if("WorkspaceActiveWindowChanged" in msg) {
+            console.log("active")
+        } else {
+            console.log("sup?")
+            throw new UnreachableError(msg)
+        }
+    }
+
 })
