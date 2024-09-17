6750d2c808361c43f4039c507b5c14d6bb948cd7
plusgut <github@webentwickler2-0.de>
2024-09-17 20:56:12 +0200

    feat(niri): impelemt stub for niri handling

--- packages/eww/index.ts
+++ packages/eww/index.ts
@@ -1,5 +1,6 @@
 import { spawn } from "node:child_process";
 
+
 const niri = spawn("niri", ["msg", "--json", "event-stream"], { serialization: "json"})
 
 type NiriEvent =
@@ -12,22 +13,69 @@ class UnreachableError extends Error {
     }
 }
 
-let previousBuffers: Buffer[] = []
+let pendingMessage = "";
 niri.stdout.on("data", (data) => {
-    previousBuffers.push(data)
-    if (data.includes("\n")) {
-        const currentBuffer = Buffer.concat(previousBuffers)
-        previousBuffers = []
-        const msg: NiriEvent = JSON.parse(currentBuffer.toString());
-
-        if ("WindowFocusChanged" in msg) {
-            console.log("focus")
-        } else if("WorkspaceActiveWindowChanged" in msg) {
-            console.log("active")
-        } else {
-            console.log("sup?")
-            throw new UnreachableError(msg)
-        }
-    }
-
+    pendingMessage += data.toString();
+    handleMessage();
 })
+
+function handleMessage() {
+    const newlinePosition = pendingMessage.indexOf("\n");
+    if (newlinePosition !== -1) {
+        const firstMessage = pendingMessage.slice(0, newlinePosition)
+        pendingMessage = pendingMessage.slice(newlinePosition + 1)
+
+        try {
+            const data = JSON.parse(firstMessage);
+            for (const key in data) {
+                if (key in niriHandler) {
+                    (niriHandler as any)[key](data[key])
+                } else {
+                    console.warn(`${key} is not yet implemented`)
+                }
+            }
+        } catch(error) {
+            console.error(error)
+        }
+        handleMessage()
+    }
+}
+
+
+type WindowsChanged = { windows: Window[] };
+type WindowFocusChanged = { id: number | null }
+type WindowClosed = { id: number }
+type KeyboardLayoutsChanged = { keyboard_layouts: { names: string[] }, current_idx: number }
+type WorkspacesChanged = { workspaces: {id: number, idx: number, name: null, output: string, is_active: boolean, is_focused: boolean, active_window_id: number }[]}
+type WindowOpenedOrChanged = { window: Window }
+type WorkspaceActiveWindowChanged = { workspace_id: number, active_window_id: number }
+type WorkspaceActivated = { id: number, focused: boolean }
+
+type Window = { id:number, title: string, app_id: string, workspace_id: number, is_focused: boolean }
+
+const niriHandler = {
+    WorkspacesChanged(_data: WorkspacesChanged) {
+
+    },
+    WindowOpenedOrChanged(_data: WindowOpenedOrChanged) {
+
+    },
+    WindowFocusChanged(_data: WindowFocusChanged) {
+
+    },
+    WorkspaceActiveWindowChanged(_data: WorkspaceActiveWindowChanged) {
+
+    },
+    WindowsChanged(_data: WindowsChanged) {
+
+    },
+    WorkspaceActivated(_data: WorkspaceActivated) {
+
+    },
+    KeyboardLayoutsChanged(data: KeyboardLayoutsChanged) {
+
+    },
+    WindowClosed(_data: WindowClosed) {
+
+    }
+}
