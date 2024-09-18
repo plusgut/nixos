ba702065dbcee62a844b79fa6804a1d9836f1444
plusgut <github@webentwickler2-0.de>
2024-09-18 12:23:23 +0200

    fix(eww): remove filtering workspaces for now

--- packages/eww/index.ts
+++ packages/eww/index.ts
@@ -1,25 +1,40 @@
 import { spawn } from "node:child_process";
 
+const niri = spawn("niri", ["msg", "--json", "event-stream"])
 
-const niri = spawn("niri", ["msg", "--json", "event-stream"], { serialization: "json"})
-
-type NiriEvent =
-    | { WindowFocusChanged: { id: number }}
-    | { WorkspaceActiveWindowChanged: { }}
-
-class UnreachableError extends Error {
-    constructor(opt: never) {
-        super(`Sould not be possible ${opt}`)
-    }
+let currentEwwState: EwwState = {
+    workspaces: [],
+    workspace_active: null
 }
 
 let pendingMessage = "";
 niri.stdout.on("data", (data) => {
     pendingMessage += data.toString();
-    handleMessage();
+    const newEwwState = handleMessage(currentEwwState);
+    const changeEwwVariables = Object.entries(newEwwState).flatMap(([key, value]) => {
+        const variableState = JSON.stringify(value)
+        if (variableState !== JSON.stringify(currentEwwState[key as keyof EwwState])) {
+            return [[key, variableState]]
+        }
+        return []
+    })
+
+    if(changeEwwVariables.length > 0) {
+        try {
+            spawn("eww", ["update", ...changeEwwVariables.map(([key, variable]) => `${key}=${variable}`)]).on("exit", (code) => {
+                if (code === 0) {
+                    currentEwwState = newEwwState
+                } else {
+                    console.log(`eww: ${code}`)
+                }
+            });
+        } catch(error) {
+            console.error(error);
+        }
+    }
 })
 
-function handleMessage() {
+function handleMessage(ewwState: EwwState) {
     const newlinePosition = pendingMessage.indexOf("\n");
     if (newlinePosition !== -1) {
         const firstMessage = pendingMessage.slice(0, newlinePosition)
@@ -29,7 +44,7 @@ function handleMessage() {
             const data = JSON.parse(firstMessage);
             for (const key in data) {
                 if (key in niriHandler) {
-                    (niriHandler as any)[key](data[key])
+                    ewwState = niriHandler[key](ewwState, data[key])
                 } else {
                     console.warn(`${key} is not yet implemented`)
                 }
@@ -37,8 +52,9 @@ function handleMessage() {
         } catch(error) {
             console.error(error)
         }
-        handleMessage()
+        return handleMessage(ewwState)
     }
+    return ewwState
 }
 
 
@@ -53,29 +69,48 @@ type WorkspaceActivated = { id: number, focused: boolean }
 
 type Window = { id:number, title: string, app_id: string, workspace_id: number, is_focused: boolean }
 
-const niriHandler = {
-    WorkspacesChanged(_data: WorkspacesChanged) {
+type EwwState = {
+    workspaces: { id: number, name: string }[]
+    workspace_active: number | null
+}
 
+const niriHandler: {[niriEvent: string]: (ewwState: EwwState, event: any) =>  EwwState} = {
+    WorkspacesChanged(ewwState, data: WorkspacesChanged) {
+        return {
+            ...ewwState,
+            workspaces: data.workspaces
+                .sort((a, b) => a.idx - b.idx)
+                .map(workspace => ({
+                    id: workspace.id,
+                    name: `${workspace.name ?? workspace.idx}`,
+                    idx: workspace.idx
+                })),
+            workspace_active: data.workspaces.find(workspace => workspace.is_active)?.idx ?? null
+        }
     },
-    WindowOpenedOrChanged(_data: WindowOpenedOrChanged) {
-
+    WindowOpenedOrChanged(ewwState, _data: WindowOpenedOrChanged) {
+        return ewwState;
     },
-    WindowFocusChanged(_data: WindowFocusChanged) {
-
+    WindowFocusChanged(ewwState, _data: WindowFocusChanged) {
+        return ewwState
     },
-    WorkspaceActiveWindowChanged(_data: WorkspaceActiveWindowChanged) {
-
+    WorkspaceActiveWindowChanged(ewwState, _data: WorkspaceActiveWindowChanged) {
+        return ewwState
     },
-    WindowsChanged(_data: WindowsChanged) {
-
+    WindowsChanged(ewwState, _data: WindowsChanged) {
+        return ewwState
     },
-    WorkspaceActivated(_data: WorkspaceActivated) {
-
+    WorkspaceActivated(ewwState, data: WorkspaceActivated) {
+        return {
+            ...ewwState,
+            workspace_active: data.id
+        }
     },
-    KeyboardLayoutsChanged(data: KeyboardLayoutsChanged) {
-
+    KeyboardLayoutsChanged(ewwState, _data: KeyboardLayoutsChanged) {
+        return ewwState
     },
-    WindowClosed(_data: WindowClosed) {
-
+    WindowClosed(ewwState, _data: WindowClosed) {
+        return ewwState
     }
 }
+
