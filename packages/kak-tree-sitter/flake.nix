f275c65fe50a1213fd8388d07724858b5792e446
plusgut <github@webentwickler2-0.de>
2024-12-27 21:09:44 +0100

    feat(kak-tree-sitter): remove unneeded inputs

--- packages/kak-tree-sitter/flake.nix
+++ packages/kak-tree-sitter/flake.nix
@@ -3,31 +3,15 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    kakoune = {
-      url = "github:mawww/kakoune";
-      flake = false;
-    };
-    kak-auto-pairs = {
-      url = "github:alexherbo2/auto-pairs.kak";
-      flake = false;
-    };
-    kak-wakatime = {
-      url = "github:WhatNodyn/kakoune-wakatime";
-      flake = false;
-    };
-    kak-active-window = {
-      url = "github:greenfork/active-window.kak";
-      flake = false;
-    };
   };
-  outputs = { self, nixpkgs, flake-utils, kakoune, kak-auto-pairs, kak-wakatime, kak-active-window }:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
 
       in {
         packages = {
-            default = pkgs.kak-tree-sitter;
+#            default = pkgs.kak-tree-sitter;
         };
     }
   );
