817f5664e9bf752a8d749c06ed8c245e20ee81af
plusgut <github@webentwickler2-0.de>
2025-08-29 20:26:53 +0200

    chore(kakoune): simplified evaluate commands

--- packages/fish/functions/kak-get-bufferfiles.fish
+++ packages/fish/functions/kak-get-bufferfiles.fish
@@ -3,7 +3,7 @@ function kak-get-bufferfiles
     set -l output $tempdir/fifo
     mkfifo $output
     # we ask the session to write the value of 'modified' for the current buffer of the list
-    printf 'try %%{ evaluate-commands -try-client "%s" %%{ echo -to-file %s %%val{buflist} } }' "client0" "$output" | kak -p $argv[1]
+    printf 'try %%{ evaluate-commands %%{ echo -to-file %s %%val{buflist} } }' "$output" | kak -p $argv[1]
     # read the value ASAP and delete FIFO to avoid lag in client
     set -l mod $(timeout 2s cat $output)
     echo $mod | tr ' ' '\n'
