12dfd478bc0d0f893eb52051ff33a5a389d82f34
plusgut <github@webentwickler2-0.de>
2025-08-30 20:55:17 +0200

    feat(kak): add cwd prefix to buffer files

--- packages/fish/functions/kak-get-bufferfiles.fish
+++ packages/fish/functions/kak-get-bufferfiles.fish
@@ -3,9 +3,9 @@ function kak-get-bufferfiles
     set -l output $tempdir/fifo
     mkfifo $output
     # we ask the session to write the value of 'modified' for the current buffer of the list
-    printf 'try %%{ evaluate-commands %%{ echo -to-file %s %%val{buflist} } }' "$output" | kak -p $argv[1]
+    printf 'try %%{ evaluate-commands %%{ echo -to-file %s %%sh{ echo $kak_buflist | awk -v RS=\'[\n ]\' \'!/^\*/{print ENVIRON["PWD"] "/" $1}\' } } }' "$output" | kak -p $argv[1]
     # read the value ASAP and delete FIFO to avoid lag in client
     set -l mod $(timeout 2s cat $output)
-    echo $mod | tr ' ' '\n'
+    echo $mod | tr ' ' '\n' 
     rm  $tempdir
 end
