38b2931ef3d8390d4448415e6e2952151a911a2e
plusgut <github@webentwickler2-0.de>
2025-09-01 08:56:12 +0200

    fix(fish): skip non sessioned kakoune directories

--- packages/fish/functions/fzf-buffer-widget.fish
+++ packages/fish/functions/fzf-buffer-widget.fish
@@ -1,6 +1,9 @@
 function fzf-buffer-widget
-    set -l result $(kak-get-bufferfiles $(kak-get-server) | string match $PWD"/*" | string sub -s $(string length $PWD"//" ) | fzf --preview "fzf-file-preview {}")
-    and commandline -rt -- (string join -- ' ' $result)
+    set -l kak_session_id $(kak-get-server)
+    if test -n "$kak_session_id"
+        set -l result $(kak-get-bufferfiles $kak_session_id | string match $PWD"/*" | string sub -s $(string length $PWD"//" ) | fzf --preview "fzf-file-preview {}")
+        and commandline -rt -- (string join -- ' ' $result)
 
-    commandline -f repaint
+        commandline -f repaint
+    end
 end
