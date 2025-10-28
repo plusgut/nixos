e6fb01edc5d8799b7b7d86741a691f4c1deec88f
plusgut <github@webentwickler2-0.de>
2025-10-28 10:50:31 +0100

    fix(foot-open): fix replacing colon to hash

--- packages/foot/scripts/foot-open.fish
+++ packages/foot/scripts/foot-open.fish
@@ -1,5 +1,15 @@
 #! /usr/bin/env fish
 
-set -l normalized_path $(echo $argv[1] | awk "/^\// {print \"file://$hostname\"\$1} ""!/^\// {print \$1}")
+set -l normalized_path $argv[1]
+
+if string match -r "^(?<path>\/[^:#]+)+([:#](?<line>[0-9]+)(:(?<column>[0-9]+))?)?\$" $normalized_path > /dev/null
+  set normalized_path "file://$hostname$path"
+  if test -n "$line"
+      set normalized_path "$normalized_path#$line"
+      if test -n "$column"
+          set normalized_path "$normalized_path:$column"
+      end
+  end
+end
 
 xdg-open $normalized_path
