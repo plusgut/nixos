6d27231f192ed216d8e8fdeb3e1be4331968ab02
plusgut <github@webentwickler2-0.de>
2025-10-28 10:16:28 +0100

    fix(foot-open): fix double print

--- packages/foot/scripts/foot-open.fish
+++ packages/foot/scripts/foot-open.fish
@@ -1,5 +1,5 @@
 #! /usr/bin/env fish
 
-set -l normalized_path $(echo $argv[1] | awk "/^\// {print \"file://$hostname\"\$1} {print \$1}")
+set -l normalized_path $(echo $argv[1] | awk "/^\// {print \"file://$hostname\"\$1} ""!/^\// {print \$1}")
 
 xdg-open $normalized_path
