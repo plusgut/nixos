449d855a5891a896516b48291a0303b0f1540c84
plusgut <github@webentwickler2-0.de>
2025-11-02 20:09:21 +0100

    feat(kakoune): add buffer-dirname to fuzzy-search prefix

--- packages/fish/config.fish
+++ packages/fish/config.fish
@@ -7,17 +7,18 @@ alias ll="lsd --long --almost-all --hyperlink auto --git"
 alias rg="rg --hyperlink-format='file://{host}{path}#{line}:{column}'"
 alias fd="fd --hyperlink=auto --hidden --exclude .git"
 
-if status is-interactive
-    set -g fish_prompt_pwd_dir_length 0
-    set -x STARSHIP_CONFIG $XDG_CONFIG_HOME/starship/config.toml
-    set -g FZF_FILE_OPTS "--preview \"fzf-file-preview \$dir/{}\""
-    set -g FZF_CTRL_T_COMMAND "fd --hidden --exclude .git --base-directory \$dir --type f --strip-cwd-prefix"
-    set -g FZF_CTRL_T_OPTS $FZF_FILE_OPTS
-    set -g FZF_ALT_C_COMMAND "fd --hidden --exclude .git --base-directory \$dir --type d --strip-cwd-prefix"
-    set -g FZF_ALT_C_OPTS $FZF_FILE_OPTS
-    set -x DIRPREVARROW 󰧀
-    set -x DIRNEXTARROW 󰧂
+set -g fish_prompt_pwd_dir_length 0
+set -x STARSHIP_CONFIG $XDG_CONFIG_HOME/starship/config.toml
+set -xg FZF_DEFAULT_OPTS "--bind \"Esc:cancel\""
+set -g FZF_FILE_OPTS "--preview \"fzf-file-preview \$dir/{}\""
+set -g FZF_CTRL_T_COMMAND "fd --hidden --exclude .git --base-directory \$dir --type f --strip-cwd-prefix"
+set -g FZF_CTRL_T_OPTS $FZF_FILE_OPTS
+set -g FZF_ALT_C_COMMAND "fd --hidden --exclude .git --base-directory \$dir --type d --strip-cwd-prefix"
+set -g FZF_ALT_C_OPTS $FZF_FILE_OPTS
+set -x DIRPREVARROW 󰧀
+set -x DIRNEXTARROW 󰧂
 
+if status is-interactive
     starship init fish | source
     zoxide init fish | source
     direnv hook fish | source
--- packages/kak/plugins/fuzzel.kak
+++ packages/kak/plugins/fuzzel.kak
@@ -9,9 +9,14 @@ define-command pick-buffers -docstring 'Select an open buffer using fuzzel' %{
 
 define-command pick-file -docstring 'Select an open buffer using fuzzel' %{
   evaluate-commands %sh{
-    BUFFER=$(fd  --hidden --exclude .git --type f | ffzf --preview 'bat --style=numbers,changes --color=always  {}')
-    if [ -n "$BUFFER" ]; then
-      printf "edit %s\n" "${BUFFER}"
+    query=""
+    if [ -n "$kak_buffile" ]; then
+      query="--query=^$(realpath --relative-base=$PWD $(dirname $kak_buffile))/ "
+    fi
+
+    buffer=$(fd  --hidden --exclude .git --type f | ffzf "$query" --preview 'bat --style=numbers,changes --color=always  {}')
+    if [ -n "$buffer" ]; then
+      printf "edit %s\n" "${buffer}"
     fi
   }
 }
