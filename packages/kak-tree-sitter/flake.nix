0f11257c1d63e70678a8a83cd01628dcd2ab151f
plusgut <github@webentwickler2-0.de>
2025-11-04 20:59:46 +0100

    chore(kak-tree-sitter): update to master

--- packages/kak-tree-sitter/flake.nix
+++ packages/kak-tree-sitter/flake.nix
@@ -2,8 +2,12 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
     flake-utils.url = "github:numtide/flake-utils";
+    kak-tree-sitter = {
+      flake = false;
+      url = "sourcehut:~hadronized/kak-tree-sitter";
+    };
   };
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, kak-tree-sitter }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
@@ -11,7 +15,11 @@
       in
       {
         packages = {
-          default = pkgs.kak-tree-sitter;
+          default = pkgs.rustPlatform.buildRustPackage {
+            name = "kak-tree-sitter";
+            src = kak-tree-sitter;
+            cargoHash = "sha256-lZNM5HqICP6JfaMiBjACcUNRTTTIRhq2ou8cOLU0yOU=";
+          };
         };
       }
     );
