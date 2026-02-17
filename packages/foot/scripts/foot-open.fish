6760467b60253cb21789e852003795981ad3d3e1
plusgut <github@webentwickler2-0.de>
2026-02-17 19:59:19 +0100

    fix(foot): fix relative paths

--- packages/foot/scripts/foot-open.fish
+++ packages/foot/scripts/foot-open.fish
@@ -2,13 +2,20 @@
 
 set -l normalized_path $argv[1]
 
-if string match -r "^(?<path>\/[^:#]+)+([:#](?<line>[0-9]+)(:(?<column>[0-9]+))?)?\$" $normalized_path > /dev/null
-  set normalized_path "file://$hostname$path"
-  if test -n "$line"
-      set normalized_path "$normalized_path#$line"
-      if test -n "$column"
-          set normalized_path "$normalized_path:$column"
-      end
+if not string match -r "^\w+://.+" $normalized_path > /dev/null
+  if string match -r "^[^/]" $normalized_path > /dev/null
+      set normalized_path "$PWD/$normalized_path"
+  end
+
+  if string match -r "^(?<path>\/[^:#]+)+([:#](?<line>[0-9]+)(:(?<column>[0-9]+))?)?\$" $normalized_path > /dev/null
+    echo match
+    set normalized_path "file://$hostname$path"
+    if test -n "$line"
+        set normalized_path "$normalized_path#$line"
+        if test -n "$column"
+            set normalized_path "$normalized_path:$column"
+        end
+    end
   end
 end
 
