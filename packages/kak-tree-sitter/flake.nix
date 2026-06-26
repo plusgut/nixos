96837e830f27946ea3bca58f84f9450ee08cf98b
plusgut <github@webentwickler2-0.de>
2026-06-26 10:03:37 +0200

    feat(kak-tree-sitter): update kak-tree-sitter

--- packages/kak-tree-sitter/flake.nix
+++ packages/kak-tree-sitter/flake.nix
@@ -18,7 +18,7 @@
           default = pkgs.rustPlatform.buildRustPackage {
             name = "kak-tree-sitter";
             src = kak-tree-sitter;
-            cargoHash = "sha256-ztVBBeLU1AByDz3yVDMZ102bDG6JfL/6IoJlcqRmCmU=";
+            cargoHash = "sha256-5h2Kvd0XAw/FbRYDAAEadJ8AqD5O+haMGEYhjWN3aFM=";
           };
         };
       }
