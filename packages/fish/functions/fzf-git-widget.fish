3294e86252f7ee1bf0cc0424e6055ae4ac5870ed
plusgut <github@webentwickler2-0.de>
2026-07-08 13:27:25 +0200

    feat(git): improve git-fzf-picker

--- packages/fish/functions/fzf-git-widget.fish
+++ packages/fish/functions/fzf-git-widget.fish
@@ -1,5 +1,5 @@
 function fzf-git-widget
-    set -l result $(git status -s | awk '{print $2}' | fzf --scheme=path --preview "fzf-preview {}")
+    set -l result $(git status -s -uall | ffzf --scheme=path  --accept-nth 2 --preview "fzf-preview {2}")
     and commandline -rt -- (string join -- ' ' $result)
 
     commandline -f repaint
