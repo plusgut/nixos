4e3ff29f5aa2dad4f2074b749ada3d541e3f595b
plusgut <github@webentwickler2-0.de>
2026-07-27 15:42:20 +0200

    feat(noctalia): improve bar

--- packages/noctalia/flake.nix
+++ packages/noctalia/flake.nix
@@ -3,7 +3,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     noctalia = {
-      url = "github:noctalia-dev/noctalia";
+      url = "github:noctalia-dev/noctalia?ref=v5.0.0-beta.5";
     };
   };
   outputs = { self, nixpkgs, flake-utils, noctalia }:
