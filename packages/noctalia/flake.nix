6983af26c1970face6c6bda1bd48185c644eef34
plusgut <github@webentwickler2-0.de>
2026-09-06 11:05:10 +0200

    chore(noctalia): switch to nixpkgs release

--- packages/noctalia/flake.nix
+++ packages/noctalia/flake.nix
@@ -2,15 +2,12 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    noctalia = {
-      url = "github:noctalia-dev/noctalia?ref=v5.0.0-beta.10";
-    };
   };
-  outputs = { self, nixpkgs, flake-utils, noctalia }:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       {
         packages = {
-          default = noctalia.packages.${system}.default;
+          default = nixpkgs.legacyPackages.${system}.noctalia;
         };
       }
     );
